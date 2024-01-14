9975dfc9da2d4553bdf289524d80f52ff847d82c
Deepansh <147298285+deepansh-maurya@users.noreply.github.com>
2024-01-14 21:52:22 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Gamecomponent } from "./Gamecomponent";
+function App() {
+  return (
+    <div className="app">
+      <Gamecomponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Tic-Tac-Toe/src/Gamecomponent.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import "./Game.css";
+export const Gamecomponent = () => {
+  const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+  ];
+  const [end, setend] = useState(true);
+  let initialArray = new Array(9).fill("  ");
+  const [turn, setturn] = useState("X");
+
+  const [result, setresult] = useState("");
+  const [turnarray, setturnarray] = useState(initialArray);
+  console.log(turnarray);
+  function handleturn(block) {
+    if (turnarray[block] == "  " && end) {
+      const newTurnArray = [...turnarray];
+      newTurnArray[block] = turn;
+      setturnarray(newTurnArray);
+      setturn(turn == "X" ? "O" : "X");
+      setresult(turn == "X" ? "O Turn" : "X Turn");
+    }
+  }
+  useEffect(() => {
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [a, b, c] = winningPatterns[i];
+      if (
+        turnarray[a] != "  " &&
+        turnarray[b] != "  " &&
+        turnarray[c] != "  "
+      ) {
+        if (
+          turnarray[a] == turnarray[b] &&
+          turnarray[b] == turnarray[c] &&
+          turnarray[c] == turnarray[a]
+        ) {
+          setresult(`${turnarray[a]} Won`);
+          setend(false);
+        }
+      }
+    }
+    if (!turnarray.includes("  ")) {
+      setresult(`Match Drawn`);
+    }
+  }, [turn, turnarray]);
+  return (
+    <>
+      <div className="wrapper">
+        <div className="board">
+          <h1>TIC TAC TOE</h1>
+          <div className="row">
+            <div onClick={() => handleturn(0)} className="block">
+              {turnarray[0]}
+            </div>
+            <div onClick={() => handleturn(1)} className="block">
+              {turnarray[1]}
+            </div>
+            <div onClick={() => handleturn(2)} className="block">
+              {turnarray[2]}
+            </div>
+          </div>
+          <div className="row">
+            <div onClick={() => handleturn(3)} className="block">
+              {turnarray[3]}
+            </div>
+            <div onClick={() => handleturn(4)} className="block">
+              {turnarray[4]}
+            </div>
+            <div onClick={() => handleturn(5)} className="block">
+              {turnarray[5]}
+            </div>
+          </div>
+          <div className="row">
+            <div onClick={() => handleturn(6)} className="block">
+              {turnarray[6]}
+            </div>
+            <div onClick={() => handleturn(7)} className="block">
+              {turnarray[7]}
+            </div>
+            <div onClick={() => handleturn(8)} className="block">
+              {turnarray[8]}
+            </div>
+          </div>
+          <div className="result">{result}</div>
+          <div>
+            {end ? (
+              <button>Game is Running.....</button>
+            ) : (
+              <button
+                onClick={() => {
+                  setend(true);
+                  setresult("");
+                  initialArray = new Array(9).fill("  ");
+                  setturn("X");
+                  setturnarray(initialArray);
+                }}
+              >
+                {` Restart the Game`}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
